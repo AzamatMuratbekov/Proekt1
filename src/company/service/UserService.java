4e4a043431b7705e22a0f6f959afa94129b1d7a8
Melloman <lovelyboyy@mail.ru>
2022-12-04 19:20:50 +0300

    Initial commit

---
+++ src/company/service/UserService.java
@@ -0,0 +1,8 @@
+package company.servise;
+
+public interface UserServise {
+    void userAdd();
+    void getId();
+    void removeId();
+    void getAllUser();
+}
---
+++ src/company/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package company.servise.impl;
+
+import company.servise.UserServise;
+
+public class UserServiceImpl implements UserServise {
+    @Override
+    public void userAdd() {
+
+    }
+
+    @Override
+    public void getId() {
+
+    }
+
+    @Override
+    public void removeId() {
+
+    }
+
+    @Override
+    public void getAllUser() {
+
+    }
+}
